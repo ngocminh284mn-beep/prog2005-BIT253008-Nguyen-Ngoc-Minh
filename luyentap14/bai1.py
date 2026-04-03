f6e9c53494df868f87dc3cfef6c86a059d2d026d
ngocminh284mn-beep <ngocminh284.mn@gmail.com>
2026-04-03 18:09:39 +0700

    Delete luyentap14 directory

--- luyentap14/bai1.py
+++
@@ -1,4 +0,0 @@
-PI = 3.14
-ban_kinh = float(input("Nhập bán kính hình tròn: "))
-chu_vi = 2 * PI * ban_kinh
-print(f"Chu vi của hình tròn có bán kính {ban_kinh} là: {chu_vi}")
--- luyentap14/bai2.py
+++
@@ -1,9 +0,0 @@
-danh_sach_ten = []
-print("Nhập tên 5 người:")
-for i in range(5):
-    ten = input(f"Người thứ {i+1}: ")
-    danh_sach_ten.append(ten)
-print("\nDanh sách sau khi nhập:", danh_sach_ten)
-if len(danh_sach_ten) >= 2:
-    del danh_sach_ten[1]
-print("Danh sách sau khi xóa :", danh_sach_ten)
